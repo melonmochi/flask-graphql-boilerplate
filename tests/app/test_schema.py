8318fb65bb7f1e258d6ddda6a489c4406c71cfd1
melonmochi <melonmochi3@gmail.com>
2019-12-07 21:31:28 +0100

    📰 Introduced new app testing context

--- tests/app/test_schema.py
+++ tests/app/test_schema.py
@@ -1,102 +1,104 @@
 from graphene.test import Client
-from app import app
 from app.schema import schema
 
 client = Client(schema)
-app.app_context().push()
 
 
-def test_departments(snapshot):
-    query = """
-        {
-          departments {
-            deptNo
-            deptName
+def test_departments(app, snapshot):
+    with app.app_context():
+        query = """
+                  {
+                    departments {
+                      deptNo
+                      deptName
+                    }
+                  }
+              """
+        snapshot.assert_match(client.execute(query))
+
+
+def test_department(app, snapshot):
+    with app.app_context():
+        query = """
+          {
+            department(deptNo: "d003"){
+              deptNo
+              deptName
+            }
           }
-        }
-    """
-    snapshot.assert_match(client.execute(query))
+      """
+        snapshot.assert_match(client.execute(query))
 
 
-def test_department(snapshot):
-    query = """
-        {
-          department(deptNo: "d003"){
-            deptNo
-            deptName
-          }
-        }
-    """
-    snapshot.assert_match(client.execute(query))
-
-
-def test_employees(snapshot):
-    query = """
-        {
-          employees(
-            filters: {
-              isManager: true
-              or: [
-                {empNoRange: { begin: "10030", end: "10050" }}
-                {lastNameIlike: "a%"}
-                {firstNameIlike: "b%"}
-                {gender: "F"},
-                {birthDateRange: { begin: "1964-02-01", end: "1974-02-01"}}
-              ]}
-            first: 10
-            sort: [LAST_NAME_ASC, FIRST_NAME_ASC]
-          ){
-            totalCount
-            edges{
-              node{
-                empNo
-                birthDate
-                firstName
-                lastName
-                gender
-                hireDate
+def test_employees(app, snapshot):
+    with app.app_context():
+        query = """
+          {
+            employees(
+              filters: {
+                isManager: true
+                or: [
+                  {empNoRange: { begin: "10030", end: "10050" }}
+                  {lastNameIlike: "a%"}
+                  {firstNameIlike: "b%"}
+                  {gender: "F"},
+                  {birthDateRange: { begin: "1964-02-01", end: "1974-02-01"}}
+                ]}
+              first: 10
+              sort: [LAST_NAME_ASC, FIRST_NAME_ASC]
+            ){
+              totalCount
+              edges{
+                node{
+                  empNo
+                  birthDate
+                  firstName
+                  lastName
+                  gender
+                  hireDate
+                }
               }
-            }
-            pageInfo{
-              hasPreviousPage,
-              hasNextPage,
-              startCursor,
-              endCursor,
-            }
-          }
-        }
-    """
-
-    # Case isManager is True
-    snapshot.assert_match(client.execute(query))
-
-    # Case isManager is False
-    query_is_manager_is_false = """
-        {
-          employees(
-            filters: {
-              isManager: false
-            }
-          ){
-            edges{
-              node{
-                empNo
+              pageInfo{
+                hasPreviousPage,
+                hasNextPage,
+                startCursor,
+                endCursor,
               }
             }
           }
-        }
-    """
-    snapshot.assert_match(client.execute(query_is_manager_is_false))
+        """
 
+        # Case isManager is True
+        snapshot.assert_match(client.execute(query))
 
-def test_employee(snapshot):
-    query = """
-        {
-          employee(empNo: "10003"){
-            empNo
-            firstName
-            lastName
+        # Case isManager is False
+        query_is_manager_is_false = """
+          {
+            employees(
+              filters: {
+                isManager: false
+              }
+            ){
+              edges{
+                node{
+                  empNo
+                }
+              }
+            }
           }
-        }
-    """
-    snapshot.assert_match(client.execute(query))
+        """
+        snapshot.assert_match(client.execute(query_is_manager_is_false))
+
+
+def test_employee(app, snapshot):
+    with app.app_context():
+        query = """
+          {
+            employee(empNo: "10003"){
+              empNo
+              firstName
+              lastName
+            }
+          }
+        """
+        snapshot.assert_match(client.execute(query))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-from app import app
+from app import app as flask_app
 from db.populate_mocks import populate_mocks
 
 
 @pytest.fixture
-def client():
-    with app.test_client() as client:
-        with app.app_context():
-            from app.models import db
-            populate_mocks(db)
-        yield client
+def app():
+    app = flask_app
+    with app.app_context():
+        from app.models import db
+        populate_mocks(db)
+    return app
