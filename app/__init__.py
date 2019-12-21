957e76ee4fa23f918cb350045330ea7823bce9b3
melonmochi <melonmochi3@gmail.com>
2019-12-21 15:32:43 +0100

    🚀 Added CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.utils import config_app
 from app.models import db
 from app.views import api
 
 app = Flask(__name__)
 config_app(app)
+CORS(app)
 db.init_app(app)
 app.register_blueprint(api)
